5c10d6459a65e924fb55d2327578e9ab77ee190b
alefemoreira <delimaalefe@gmail.com>
2024-03-29 08:29:25 -0300

    object format

--- lib/pokemometro.ex
+++ lib/pokemometro.ex
@@ -1,5 +1,5 @@
 defmodule Pokemometro do
-  @url "https://pokeapi.co/api/v2/pokemon/ditto"
+  @url "https://pokeapi.co/api/v2/pokemon/"
   @moduledoc """
   Documentation for `Pokemometro`.
   """
@@ -13,14 +13,73 @@ defmodule Pokemometro do
       :world
 
   """
-  def get_pokemon do
-    HTTPoison.get(@url)
-    |> format_data
+  def main(_) do
+    get_pokemon("pikachu")
+    |> print_pokemon
   end
 
+  def print_pokemon({:ok, pokemon}) do
+    IO.puts("---------- " <> String.capitalize(pokemon[:name]) <> " ----------")
+    IO.puts("ID        " <> " ::: " <> Integer.to_string(pokemon[:id]))
+    IO.puts("Altura (m)" <> " ::: " <> Float.to_string(pokemon[:height]) <> "m")
+    IO.puts("Peso (kg) " <> " ::: " <> Float.to_string(pokemon[:weight]) <> "kg")
 
-  def format_data({:ok, %HTTPoison.Response{status_code: 200, body: data}}) do
-    {:ok, Poison.decode(data)}
+    IO.puts("--------- Estatísticas --------")
+  end
+
+  def get_pokemon(name) do
+    HTTPoison.get(@url <> name)
+    |> format_data
+    |> filter_data
+
+    # |> print_pokemon
+
+    # {:ok, %{}}
+  end
+
+  def list2string([], _), do: ""
+  def list2string([x | r], sep \\ ", "), do: x <> sep <> list2string(r, sep)
+
+  def format_stats(obj, []), do: obj
+
+  def format_stats(obj, [stat | stats]) do
+    Map.put(obj, stat["stat"]["name"], stat["base_stat"])
+    |> format_stats(stats)
+  end
+
+  def format_types([]), do: []
+
+  def format_types([tt | types]) do
+    [tt["type"]["name"] | format_types(types)]
+  end
+
+  @spec filter_data({:ok, nil | maybe_improper_list() | map()}) ::
+          {:ok,
+           %{
+             evolutions: <<_::24>>,
+             height: any(),
+             id: any(),
+             name: any(),
+             stats: any(),
+             type: any(),
+             weight: any()
+           }}
+  def filter_data({:ok, data}) do
+    {:ok,
+     %{
+       id: data["id"],
+       name: data["name"],
+       height: data["height"] / 10,
+       weight: data["weight"] / 10,
+       stats: format_stats(%{}, data["stats"]),
+       types: format_types(data["types"]),
+       evolutions: "sfd"
+     }}
+  end
+
+  def format_data({:ok, %HTTPoison.Response{status_code: status_code, body: data}})
+      when status_code >= 200 and status_code < 300 do
+    Poison.decode(data)
   end
 
   def format_data({:ok, %HTTPoison.Response{status_code: _, body: data}}) do
@@ -28,5 +87,4 @@ defmodule Pokemometro do
   end
 
   def format_data({:erro, r}), do: {:erro, r}
-
 end
